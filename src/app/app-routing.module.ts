396356d974792adf39b1664e94cadeab88255164
Donald MacRury <51086456+Swindelllist@users.noreply.github.com>
2021-03-27 11:16:34 +0000

    child routes properly displaying

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AuthGuard } from './auth/auth.guard';
 import { ContractorsComponent } from './home/contractors/contractors.component';
 import { CustomersComponent } from './home/customers/customers.component';
 import { HomeComponent } from './home/home.component';
@@ -8,8 +9,8 @@ import { HomeComponent } from './home/home.component';
 const routes: Routes = [
   { path: 'home', component: HomeComponent,
     children: [
-      { path: 'home/customers', component: CustomersComponent },
-      { path: 'home/contractors', component: ContractorsComponent },
+      { path: 'customers', component: CustomersComponent, canActivate: [AuthGuard] },
+      { path: 'contractors', component: ContractorsComponent, canActivate: [AuthGuard] },
     ]
   },
   { path: '', redirectTo: '/home', pathMatch: 'full'}
