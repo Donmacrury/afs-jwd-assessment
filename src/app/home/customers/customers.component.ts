4e4d5712df2bdab60073359fb7fe10495cc93d71
Donald MacRury <51086456+Swindelllist@users.noreply.github.com>
2021-03-27 19:34:37 +0000

    filter for gender working

--- src/app/home/customers/customers.component.ts
+++ src/app/home/customers/customers.component.ts
@@ -1,10 +1,14 @@
 import { Component, OnInit } from '@angular/core';
-import { NzTableSortFn, NzTableSortOrder } from 'ng-zorro-antd/table';
+import { NzTableFilterFn, NzTableSortFn, NzTableSortOrder, NzTableFilterList } from 'ng-zorro-antd/table';
 import * as data from '../../../assets/mock_data_(2).json';
 
 interface ColumnItem {
   name: string;
+  sortOrder: NzTableSortOrder | null;
   sortFn: NzTableSortFn | null;
+  filteredList: NzTableFilterList;
+  filterFn: NzTableFilterFn | null;
+  filterMultiple: boolean;
   sortDirections: NzTableSortOrder[];
 }
 
@@ -22,8 +26,54 @@ export class CustomersComponent implements OnInit {
   columnsList: ColumnItem[] = [
     {
       name: 'First Name',
+      sortOrder: null,
+      sortFn: (a: any, b: any) => a.first_name.localeCompare(b.first_name),
       sortDirections: ['ascend', 'descend', null],
-      sortFn: (a: any, b: any) => a.first_name.localeCompare(b.first_name)
+      filterMultiple: false,
+      filteredList: [],
+      filterFn: null,
+    },
+    {
+      name: 'Last Name',
+      sortOrder: null,
+      sortFn: null,
+      sortDirections: [null],
+      filteredList: null,
+      filterFn: null,
+      filterMultiple: false
+    },
+    {
+      name: 'Car',
+      sortOrder: null,
+      sortDirections: [null],
+      sortFn: null,
+      filteredList: [null],
+      filterFn: null,
+      filterMultiple: false
+    },
+    {
+      name: 'No Claims Bonus',
+      sortOrder: null,
+      sortDirections: [null],
+      sortFn: null,
+      filterMultiple: false,
+      filteredList: [
+        { text: 'True', value: true },
+        { text: 'False', value: false }
+      ],
+      filterFn: (no_claims: boolean, item: any) => item.no_claims.indexOf(no_claims) !== -1
+    },
+    {
+      name: 'Gender',
+      sortOrder: null,
+      sortDirections: [null],
+      sortFn: null,
+      filteredList: [
+        { text: 'Female', value: 'Female'},
+        { text: 'Male', value: 'Male' }
+      ],
+      filterFn: (gender: string[], item: any) => gender.some(gender => item.gender.indexOf(gender) !== -1),
+      filterMultiple: false
     }
   ]
 
