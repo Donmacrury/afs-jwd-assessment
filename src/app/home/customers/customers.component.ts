5b00668b76531dce94c120f3772a81735234110a
Donald MacRury <51086456+Swindelllist@users.noreply.github.com>
2021-03-27 11:45:46 +0000

    mock_data_(2) corectly resolved

--- src/app/home/customers/customers.component.ts
+++ src/app/home/customers/customers.component.ts
@@ -1,16 +1,22 @@
 import { Component, OnInit } from '@angular/core';
-import * as data from '../../../assets/mock_data_(2)';
+import * as data from '../../../assets/mock_data_(2).json';
 
 @Component({
   selector: 'app-customers',
   templateUrl: './customers.component.html',
   styleUrls: ['./customers.component.scss']
 })
+
+
 export class CustomersComponent implements OnInit {
+
+  listData: data
 
   constructor() { }
 
   ngOnInit(): void {
+    console.log(this.listData);
+    
   }
 
 }
